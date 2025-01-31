1182ceb130d826ab72762fcae12dea31ae40e712
User <User@WRSD-LD309-026.dut.ac.za>
2025-01-31 13:34:38 +0200

    asyncStorage.js added

---
+++ asyncStorage.js
@@ -0,0 +1,10 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const loadStateFromStorage = async () => {
+    const state = await AsyncStorage.getItem("shoppingLists")
+    return state ? JSON.parse(state) : []
+}
+
+const saveStateToStorage = async (state) => {
+    await AsyncStorage.setItem("shoppingLists", JSON.stringify(state))
+}
