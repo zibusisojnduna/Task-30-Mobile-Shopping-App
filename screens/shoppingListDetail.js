12ecaa507394da95a13e5d8f5b984cc31add1ce0
User <User@WRSD-LD309-026.dut.ac.za>
2025-01-31 13:31:08 +0200

    shoppingListDispllay.js added

--- screens/shoppingListDetail.js
+++ screens/shoppingListDetail.js
@@ -5,18 +5,59 @@ import { addItem, deleteItem, editItem } from "react";
 
 function ShoppingListDetail({ route }) {
     const { listId } = route.params
-    const shoppingList = useSelector((state) => 
-        state.shoppingList.find()
+    const dispatch = useDispatch()
+    const shoppingList = useSelector((state) => {
+        state.shoppingLists.find((list) => list.id === listId)
+    })
+
+    const [newItemName, setNewItemName] =useState("")
+    const [newItemQuantity, setNewItemQuantity] = useState("")
+
+    const handleAddItem = () => {
+        dispatch(addItem(listId, {name: newItemName, quantity: newItemQuantity, purchased: false}))
+        setNewItemName("")
+        setNewItemQuantity("")
+    }
+
+    const handleTogglePurchased = (itemId) => {
+        dispatch(toggleItemPurchased(listId, itemId))
+    }
+
+    const handleDeleteItem = (itemId) => {
+        dispatch(deleteItem(listId, itemId))
+    }
+
+    return(
+        <View>
+            <TextInput
+                value={newItemName}
+                onChangeText={setNewItemName}
+                placeholder="Item Name"
+            />
+            <TextInput
+                value={newItemQuantity}
+                onChangeText={setNewItemQuantity}
+                placeholder="Quantity"
+            />
+            <Button title="Add Item" onPress={handleAddItem} />
+
+            <FlatList
+            data={shoppingList.items}
+            renderItem={({ item }) => (
+                <View>
+                    <Text>{item.name} - {item.quantity}</Text>
+                    <Button
+                        title={item.purchased ? "Mark as Unpurchased" : "Mark as Purchased"}
+                        onPress={() => handleTogglePurchased(item.id)}
+                        />
+                        <Button title="Delete" onPress={() => handleDeleteItem(item.id)} />
+                </View>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            />
+        </View>
     )
 } 
 
-
-
-
-function ShoppingListDetail() {
-    return(
-        <></>
-    )
-}
 
 export default ShoppingListDetail
